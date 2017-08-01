c14ca4588277d664642b9bee3a55b570a0674f19
goedzo <git@nims.nl>
2017-08-01 11:17:12 +0200

    0.69.1 update

--- pgoapi/protos/pogoprotos/networking/responses/gym_feed_pokemon_response_pb2.py
+++ pgoapi/protos/pogoprotos/networking/responses/gym_feed_pokemon_response_pb2.py
@@ -22,7 +22,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='pogoprotos/networking/responses/gym_feed_pokemon_response.proto',
   package='pogoprotos.networking.responses',
   syntax='proto3',
-  serialized_pb=_b('\n?pogoprotos/networking/responses/gym_feed_pokemon_response.proto\x12\x1fpogoprotos.networking.responses\x1a-pogoprotos/data/badge/awarded_gym_badge.proto\x1a\x32pogoprotos/data/gym/gym_status_and_defenders.proto\x1a(pogoprotos/enums/pokemon_family_id.proto\"\xcc\x05\n\x16GymFeedPokemonResponse\x12N\n\x06result\x18\x01 \x01(\x0e\x32>.pogoprotos.networking.responses.GymFeedPokemonResponse.Result\x12L\n\x18gym_status_and_defenders\x18\x02 \x01(\x0b\x32*.pogoprotos.data.gym.GymStatusAndDefenders\x12\x39\n\tgym_badge\x18\x03 \x01(\x0b\x32&.pogoprotos.data.badge.AwardedGymBadge\x12\x18\n\x10stardust_awarded\x18\x04 \x01(\x05\x12\x12\n\nxp_awarded\x18\x05 \x01(\x05\x12\x19\n\x11num_candy_awarded\x18\x06 \x01(\x05\x12:\n\x0f\x63\x61ndy_family_id\x18\x07 \x01(\x0e\x32!.pogoprotos.enums.PokemonFamilyId\x12\x19\n\x11\x63ooldown_complete\x18\x08 \x01(\x03\"\xb8\x02\n\x06Result\x12\t\n\x05UNSET\x10\x00\x12\x0b\n\x07SUCCESS\x10\x01\x12\x14\n\x10\x45RROR_CANNOT_USE\x10\x02\x12\x16\n\x12\x45RROR_NOT_IN_RANGE\x10\x03\x12\x1b\n\x17\x45RROR_POKEMON_NOT_THERE\x10\x04\x12\x16\n\x12\x45RROR_POKEMON_FULL\x10\x05\x12\x19\n\x15\x45RROR_NO_BERRIES_LEFT\x10\x06\x12\x14\n\x10\x45RROR_WRONG_TEAM\x10\x07\x12\x15\n\x11\x45RROR_WRONG_COUNT\x10\x08\x12\x12\n\x0e\x45RROR_TOO_FAST\x10\t\x12\x16\n\x12\x45RROR_TOO_FREQUENT\x10\n\x12\x12\n\x0e\x45RROR_GYM_BUSY\x10\x0b\x12\x15\n\x11\x45RROR_RAID_ACTIVE\x10\x0c\x12\x14\n\x10\x45RROR_GYM_CLOSED\x10\rb\x06proto3')
+  serialized_pb=_b('\n?pogoprotos/networking/responses/gym_feed_pokemon_response.proto\x12\x1fpogoprotos.networking.responses\x1a-pogoprotos/data/badge/awarded_gym_badge.proto\x1a\x32pogoprotos/data/gym/gym_status_and_defenders.proto\x1a(pogoprotos/enums/pokemon_family_id.proto\"\xd4\x05\n\x16GymFeedPokemonResponse\x12N\n\x06result\x18\x01 \x01(\x0e\x32>.pogoprotos.networking.responses.GymFeedPokemonResponse.Result\x12L\n\x18gym_status_and_defenders\x18\x02 \x01(\x0b\x32*.pogoprotos.data.gym.GymStatusAndDefenders\x12\x41\n\x11\x61warded_gym_badge\x18\x03 \x01(\x0b\x32&.pogoprotos.data.badge.AwardedGymBadge\x12\x18\n\x10stardust_awarded\x18\x04 \x01(\x05\x12\x12\n\nxp_awarded\x18\x05 \x01(\x05\x12\x19\n\x11num_candy_awarded\x18\x06 \x01(\x05\x12:\n\x0f\x66\x61mily_candy_id\x18\x07 \x01(\x0e\x32!.pogoprotos.enums.PokemonFamilyId\x12\x19\n\x11\x63ooldown_complete\x18\x08 \x01(\x03\"\xb8\x02\n\x06Result\x12\t\n\x05UNSET\x10\x00\x12\x0b\n\x07SUCCESS\x10\x01\x12\x14\n\x10\x45RROR_CANNOT_USE\x10\x02\x12\x16\n\x12\x45RROR_NOT_IN_RANGE\x10\x03\x12\x1b\n\x17\x45RROR_POKEMON_NOT_THERE\x10\x04\x12\x16\n\x12\x45RROR_POKEMON_FULL\x10\x05\x12\x19\n\x15\x45RROR_NO_BERRIES_LEFT\x10\x06\x12\x14\n\x10\x45RROR_WRONG_TEAM\x10\x07\x12\x15\n\x11\x45RROR_WRONG_COUNT\x10\x08\x12\x12\n\x0e\x45RROR_TOO_FAST\x10\t\x12\x16\n\x12\x45RROR_TOO_FREQUENT\x10\n\x12\x12\n\x0e\x45RROR_GYM_BUSY\x10\x0b\x12\x15\n\x11\x45RROR_RAID_ACTIVE\x10\x0c\x12\x14\n\x10\x45RROR_GYM_CLOSED\x10\rb\x06proto3')
   ,
   dependencies=[pogoprotos_dot_data_dot_badge_dot_awarded__gym__badge__pb2.DESCRIPTOR,pogoprotos_dot_data_dot_gym_dot_gym__status__and__defenders__pb2.DESCRIPTOR,pogoprotos_dot_enums_dot_pokemon__family__id__pb2.DESCRIPTOR,])
 
@@ -93,8 +93,8 @@ _GYMFEEDPOKEMONRESPONSE_RESULT = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   options=None,
-  serialized_start=646,
-  serialized_end=958,
+  serialized_start=654,
+  serialized_end=966,
 )
 _sym_db.RegisterEnumDescriptor(_GYMFEEDPOKEMONRESPONSE_RESULT)
 
@@ -121,7 +121,7 @@ _GYMFEEDPOKEMONRESPONSE = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='gym_badge', full_name='pogoprotos.networking.responses.GymFeedPokemonResponse.gym_badge', index=2,
+      name='awarded_gym_badge', full_name='pogoprotos.networking.responses.GymFeedPokemonResponse.awarded_gym_badge', index=2,
       number=3, type=11, cpp_type=10, label=1,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
@@ -149,7 +149,7 @@ _GYMFEEDPOKEMONRESPONSE = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='candy_family_id', full_name='pogoprotos.networking.responses.GymFeedPokemonResponse.candy_family_id', index=6,
+      name='family_candy_id', full_name='pogoprotos.networking.responses.GymFeedPokemonResponse.family_candy_id', index=6,
       number=7, type=14, cpp_type=8, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
@@ -176,13 +176,13 @@ _GYMFEEDPOKEMONRESPONSE = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=242,
-  serialized_end=958,
+  serialized_end=966,
 )
 
 _GYMFEEDPOKEMONRESPONSE.fields_by_name['result'].enum_type = _GYMFEEDPOKEMONRESPONSE_RESULT
 _GYMFEEDPOKEMONRESPONSE.fields_by_name['gym_status_and_defenders'].message_type = pogoprotos_dot_data_dot_gym_dot_gym__status__and__defenders__pb2._GYMSTATUSANDDEFENDERS
-_GYMFEEDPOKEMONRESPONSE.fields_by_name['gym_badge'].message_type = pogoprotos_dot_data_dot_badge_dot_awarded__gym__badge__pb2._AWARDEDGYMBADGE
-_GYMFEEDPOKEMONRESPONSE.fields_by_name['candy_family_id'].enum_type = pogoprotos_dot_enums_dot_pokemon__family__id__pb2._POKEMONFAMILYID
+_GYMFEEDPOKEMONRESPONSE.fields_by_name['awarded_gym_badge'].message_type = pogoprotos_dot_data_dot_badge_dot_awarded__gym__badge__pb2._AWARDEDGYMBADGE
+_GYMFEEDPOKEMONRESPONSE.fields_by_name['family_candy_id'].enum_type = pogoprotos_dot_enums_dot_pokemon__family__id__pb2._POKEMONFAMILYID
 _GYMFEEDPOKEMONRESPONSE_RESULT.containing_type = _GYMFEEDPOKEMONRESPONSE
 DESCRIPTOR.message_types_by_name['GymFeedPokemonResponse'] = _GYMFEEDPOKEMONRESPONSE
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
